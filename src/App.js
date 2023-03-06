af6aca5ffad3865f220ff107a38309c3c1d989ea
matthew <larochelle.matthew@gmail.com>
2023-03-06 10:25:06 -0800

    best section complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Best from './components/best/Best';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Best />
     </>
   );
 }
